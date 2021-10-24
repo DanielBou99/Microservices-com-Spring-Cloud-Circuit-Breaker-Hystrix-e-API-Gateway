ca0d5dc44f7c82ff0930237ee0e43719c408a432
Daniel Bou <danielbou@outlook.com.br>
2021-10-24 18:10:56 -0300

    Projeto Inicial

---
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -0,0 +1,44 @@
+package br.com.alura.microservice.loja.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.alura.microservice.loja.client.FornecedorClient;
+import br.com.alura.microservice.loja.controller.dto.CompraDTO;
+import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
+import br.com.alura.microservice.loja.controller.dto.InfoPedidoDTO;
+import br.com.alura.microservice.loja.modelo.Compra;
+
+@Service
+public class CompraService {
+	
+	
+	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
+
+	@Autowired
+	private FornecedorClient fornecedorClient;
+	
+	public Compra realizaCompra(CompraDTO compra) {
+		
+		final String estado = compra.getEndereco().getEstado();
+		
+		LOG.info("Buscando informações do fornecedor de {}.", estado);
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		
+		LOG.info("Realizando um pedido.");
+		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
+
+		Compra compraSalva = new Compra();
+		compraSalva.setPedidoId(pedido.getId());
+		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
+		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+		
+		return compraSalva;
+	}
+
+}
